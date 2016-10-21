b4342e2de8d569a92b6121533cb671723a7ebc5e
Jack Pincombe <jackpincombe@hotmail.co.uk>
2016-10-21 16:47:45 +0100

    working paradim

--- paradim.php
+++ paradim.php
@@ -4,6 +4,11 @@
  * User: jackpincombe
  * Date: 21/10/2016
  * Time: 16:26
+ * For an interview I had to return true id a word was a paradim
+ * and of course the next day after looking at some php syntax I was able to
+ * achieve this
+ *
+ * FFS
  */
 
 $string = "jack";
@@ -16,3 +21,5 @@ if( mb_strtoupper($string) == mb_strtoupper($x)){
 
     echo $string . " is not a paradim";
 }
+
+
