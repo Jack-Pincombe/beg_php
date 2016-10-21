029310b74b0943926c95d059b572ae1bf09f70fc
Jack Pincombe <jackpincombe@hotmail.co.uk>
2016-10-21 15:43:55 +0100

    Just a basic intro int php

---
+++ 121.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jackpincombe
+ * Date: 21/10/2016
+ * Time: 12:53
+ */
+
+/**
+//Users information
+$name = "Jack Pincombe";
+$email = "jackpincombe@hotmail.co.uk";
+
+//displaying the users information
+echo $name ,'s ' , 'email is ' , $email;
+ *
+ * */
+
+$firstName = 'Jack';
+$lastName = 'Pincombe';
+$title = '"The Hitchhicker\'s guide to the galaxy"';
+$author = 'Douglas Adams';
+$answer = 420;
+$newlines = "\r\n\r\n";
+
+$fullName = $firstName . ' ' . $lastName;
+$book = "$title by $author";
+
+
+echo $fullName . '<br>';
+echo $book;
+ /**
+
+?>
+
+<!DOCTYPE html>
+    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Embedded software</title>
+
+</head>
+<body>
+<h1>Dynamic text In Action</h1>
+<p>There is o need for everything in a PHP file to be store in variables and displayed using echo.
+PHP code is designed to be embedded in HTML</p>
+<p><mark><?= $book; ?></mark> is a comedy sci-fi
+series originally written by <mark><?php echo $author; ?></mark>, and
+subsenquently turned into a book, TB series, movie and numerous stage shows. It
+Reveals that the Answer to the Ultimate Question of Life, The universe, and
+Everything is <mark><?php echo $answer; ?></mark></p>
+
+</body>
+
+</html>
+
+  **/
