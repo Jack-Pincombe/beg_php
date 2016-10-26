9510eef815dd7c7894dfbbc660192527bea51659
Jack Pincombe <jackpincombe@hotmail.co.uk>
2016-10-26 20:21:48 +0100

    Just adding an example of switch statements

---
+++ switch.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jackpincombe
+ * Date: 26/10/2016
+ * Time: 20:12
+ */
+
+$name = 'android';
+
+switch ($name) {
+    case 'Arthur Dent':
+        echo "You are called Arty Dent";
+        break;
+    case 'Jack Pincombe':
+        echo "Your called Jack Pincombe";
+        break;
+    case 'Marvin';
+    case "android":
+        echo "I've got a terible pain in on my diodes down my left side";
+        break;
+
+    default:
+        "I dunno who you are";
+}
