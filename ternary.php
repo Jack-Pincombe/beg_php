b40bf3fc7fbb5c77a00b0122e632ca9c59f82c58
Jack Pincombe <jackpincombe1@gmail.com>
2016-10-27 18:30:00 +0100

    Simple ternary example not sure if ill ever use

---
+++ ternary.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jackpincombe
+ * Date: 27/10/2016
+ * Time: 18:26
+ */
+
+
+// $var = (condition) ? value if true: value if false;
+
+$answer = 40 ;
+
+$resut = ($answer == 42 ) ? 'The answer to the Qustion of the Life, 
+the Universe and everything': 'Keep calculating';
+
+echo $resut;
