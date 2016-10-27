308953b6c78c7e26426534e17c5d82eb7e130f19
Jack Pincombe <jackpincombe1@gmail.com>
2016-10-27 18:40:28 +0100

    Ternary operater ??

---
+++ t1.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jackpincombe
+ * Date: 27/10/2016
+ * Time: 18:38
+ */
+
+$unit = 0;
+
+$wholesale = $unit ?? 25;
+
+
+echo $wholesale;
--- ternary.php
+++ ternary.php
@@ -10,8 +10,12 @@
 // $var = (condition) ? value if true: value if false;
 
 $answer = 40 ;
+$unit_cost = 0;
+
 
 $resut = ($answer == 42 ) ? 'The answer to the Qustion of the Life, 
 the Universe and everything': 'Keep calculating';
 
-echo $resut;
+$wholesale_price = $unit_cost ?: 25;
+
+echo $wholesale_price;
