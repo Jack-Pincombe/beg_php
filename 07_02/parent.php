fd89aa6a6cf1628b1d2d7e04379d43179dcec4aa
Jack Pincombe <jackpincombe1@gmail.com>
2016-11-09 17:40:02 +0000

    creating php links

---
+++ 07_02/parent.php
@@ -0,0 +1,19 @@
+<?php $siteroot = '/php/beg_php/07_02' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Using server-side includes</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Including External Files</h1>
+<p>This paragraph is in the original file.</p>
+<?php include 'includes/para.html'?>
+<p>This is also in the original file.</p>
+<?php include 'includes/para.html'?>
+<?php require 'includes/copyright.php'?>
+<p><?= lyn_copyright(2014);?> Jack Pincombe</p>
+</body>
+</html>
